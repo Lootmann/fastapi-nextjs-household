e76bba0e1ad33e63c9b0bfb8fe2444a0734e0b4c
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-02-21 00:23:55 +0900

    backend: tests - conftest for get login access_token

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,15 +1,30 @@
+from typing import Tuple
+
 import pytest
 
 from api.models import users as user_model
-from api.schemas import users as user_schema
-from tests.factories import UserFactory, random_string
+from api.schemas import auths as auth_schema
+from tests.factories import random_string
 from tests.init_async_client import async_client as client
 
 
 @pytest.fixture
 @pytest.mark.asyncio
-async def user_fixture(client):
+async def login_fixture(client) -> Tuple[user_model.User, dict]:
+    """login_fixture
+    create user and login by the created user
+    """
     user = user_model.User(name=random_string(), password=random_string())
     resp = await client.post("/users", json={"name": user.name, "password": user.password})
     user.id = resp.json()["id"]
-    return user
+
+    resp = await client.post(
+        "/token",
+        data={"username": user.name, "password": user.password},
+        headers={"content-type": "application/x-www-form-urlencoded"},
+    )
+
+    token = auth_schema.TokenData(**resp.json())
+    headers = {"Authorization": f"Bearer {token.access_token}"}
+
+    return user, headers
